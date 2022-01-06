3e560fc2a29070aa7d551147c5c194955c303425
emcydeveloper <emcydeveloper@gmail.com>
2022-01-07 04:46:06 +0530

    Add mivie with MUI and summary Hide and Unhide

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
 
 
 export default function App() {
   const movieInfo = [
-    {Name: "Vinnaithaandi Varuvaayaa", Ratings: "10", Image: "https://upload.wikimedia.org/wikipedia/en/7/78/Vtvalbumcover.jpg", Summary: "A Hindu assistant director, Karthik, falls in love with Jessie, a Christian from a traditional family. Things change when Karthik becomes busy during a forty-day shoot in Goa."},
+    {Name: "Doctor", Ratings: "10", Image: "https://static.toiimg.com/photo/80667091.jpeg", Summary: "A military doctor embarks on a journey to track down his former fiancee's kidnapped niece. His mission then leads him to the heart of a human trafficking ring in Goa."},
     {Name: "Sethupathi", Ratings: "10", Image: "https://images-na.ssl-images-amazon.com/images/I/81kFniN14wL._RI_.jpg", Summary: "While conducting an investigation into the mysterious death of a fellow officer, a cop's own reputation is questioned when a suspect in his custody is shot."},
     {Name: "Alai Payuthey", Ratings: "10", Image: "https://static-koimoi.akamaized.net/wp-content/new-galleries/2021/04/koimoi-recommends-alaipayuthey-starring-r-madhavan-shalini-003.jpg", Summary: "Karthik and Shakti marry against the wishes of their families and start their new life. However, they soon realise that marriage is not the bed of roses they had imagined it would be."},
-    {Name: "Doctor", Ratings: "10", Image: "https://static.toiimg.com/photo/80667091.jpeg", Summary: "A military doctor embarks on a journey to track down his former fiancee's kidnapped niece. His mission then leads him to the heart of a human trafficking ring in Goa."},
-    {Name: "Fast & Furious", Ratings: "10", Image: "https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg", Summary: "Fast & Furious is a media franchise centered on a series of action films that are largely concerned with illegal street racing, heists, spies and family. The franchise also includes short films, a television series, live shows, video games and theme park attractions. It is distributed by Universal Pictures."}
+    {Name: "Fast & Furious", Ratings: "7", Image: "https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg", Summary: "Fast & Furious is a media franchise centered on a series of action films that are largely concerned with illegal street racing, heists, spies and family. The franchise also includes short films, a television series, live shows, video games and theme park attractions. It is distributed by Universal Pictures."}
   ];
   const [Name,setName]=useState("");
   const [Ratings,setRatings]=useState("");
@@ -26,11 +28,17 @@ export default function App() {
       <input value={Ratings} onChange={(event)=>setRatings(event.target.value)} placeholder ="Ratings"/>
       <input value={Image} onChange={(event)=>setImage(event.target.value)} placeholder ="Image URL"/>
       <input value={Summary} onChange={(event)=>setSummary(event.target.value)} placeholder ="Summary"/>
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
         const newMovie = {Name:Name,Ratings:Ratings,Image:Image,Summary:Summary};
         setMovieList([...movieList,newMovie])
-        }}>Add Movie</button>
+        }}>Add Movie</button> */}
+        <Button variant="contained" onClick={()=>{
+        const newMovie = {Name:Name,Ratings:Ratings,Image:Image,Summary:Summary};
+        setMovieList([...movieList,newMovie])
+        }}><AddIcon />Add Movie</Button>
+        {/* <button type="button" class="btn btn-primary">Primary</button> */}
       </div>
+
       <section className="main-container">
       {movieList.map((movie) => (
         <Movie Name={movie.Name} Ratings={movie.Ratings} Image={movie.Image} Summary={movie.Summary} />
@@ -42,6 +50,17 @@ export default function App() {
 }
 
 function Movie({Name,Ratings,Image,Summary}) {
+  const ratingStyles={color: Ratings<8 ? "red" : "green"};
+  let [summarydisplayValue, setdisplaySummaryValue]=useState("none");
+  let summaryStyles = {display:summarydisplayValue}
+  
+  let [fabUnhide, setFabUnhide] = useState("block")
+  let fabStyleUnhide = {display:fabUnhide};
+
+  let [fabHide, setFabHide] = useState("none")
+  let fabStyleHide = {display:fabHide,float:"right"};
+  
+
   return (
     <div className="movie-container">
       <img className="movie-poster" src={Image} alt=""></img>
@@ -49,10 +68,26 @@ function Movie({Name,Ratings,Image,Summary}) {
       <hr></hr>
       <section className="movie-section">
         <h3 className="movie-name">{Name}</h3>
-        <p className="movie-rating">⭐ {Ratings}</p>
+        <p className="movie-rating" style={ratingStyles}>⭐ {Ratings}</p>
       </section>
       <hr></hr>
-      <p className="movie-summary">{Summary}</p>
+      <Fab style={fabStyleUnhide} size="small" color="secondary" aria-label="add" onClick={() => 
+        {
+          setdisplaySummaryValue(summarydisplayValue === "none" ? "block" : "none");
+          setFabUnhide(fabUnhide === "block" ? "none" : "block");
+          setFabHide(fabHide === "none" ? "block" : "none");
+        }}>
+      + 
+      </Fab>
+      <p className="movie-summary" style={summaryStyles}>{Summary}</p>
+      <Fab style={fabStyleHide} size="small" color="secondary" aria-label="add" onClick={() => 
+        {
+          setdisplaySummaryValue(summarydisplayValue === "block" ? "none" : "block");
+          setFabUnhide(fabUnhide === "none" ? "block" : "none");
+          setFabHide(fabHide === "block" ? "none" : "block");
+        }}>
+      -
+      </Fab>
     </div>
   );
 }
