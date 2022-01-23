4721f86177253203dc1f5788ede192cc796af9af
emcydeveloper <emcydeveloper@gmail.com>
2022-01-23 16:52:50 +0530

    useHistory

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
 import { Movie } from "./Movie";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, useParams } from "react-router-dom";
 
 export default function App() {
   const movieInfo = [
@@ -56,154 +56,165 @@ export default function App() {
 
   return (
     <div className="App">
-
       <ul>
-      <li>
-          <Link to ="/">Home</Link>
+        <li>
+          <Link to="/">Home</Link>
         </li>
         <li>
-        <Link to="/movies">Name of the link</Link>
+          <Link to="/movies">List Movies</Link>
         </li>
-        
       </ul>
       <Switch>
+        <Route path="/movies/:movieid">
+          <MovieDetils movieList = {movieList} />
+        </Route>
         <Route path="/movies">
           <h1>testing</h1>
           <div className="btnWrapper">
-        <Button
-          className="btnCentered"
-          style={addBtnStyle}
-          variant="contained"
-          onClick={() => {
-            setAddMovieBtn(addMovieBtn === "none" ? "block" : "none");
-            setAddBtn(addBtn === "block" ? "none" : "block");
-          }}
-        >
-          <AddIcon />
-          Click to add Movie
-        </Button>
-      </div>
+            <Button
+              className="btnCentered"
+              style={addBtnStyle}
+              variant="contained"
+              onClick={() => {
+                setAddMovieBtn(addMovieBtn === "none" ? "block" : "none");
+                setAddBtn(addBtn === "block" ? "none" : "block");
+              }}
+            >
+              <AddIcon />
+              Click to add Movie
+            </Button>
+          </div>
 
-      <div className="add-movie-form" style={addMovieStyles}>
-        <h1>Enter Movie Details</h1>
-        <TextField
-          required
-          className="add-movie-txt"
-          id="outlined-required"
-          label="Name"
-          defaultValue="Name"
-          value={Name}
-          onChange={(event) => setName(event.target.value)}
-          placeholder="Name"
-        />
-        {/* <input
+          <div className="add-movie-form" style={addMovieStyles}>
+            <h1>Enter Movie Details</h1>
+            <TextField
+              required
+              className="add-movie-txt"
+              id="outlined-required"
+              label="Name"
+              value={Name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder="Name"
+            />
+            {/* <input
           value={Name}
           onChange={(event) => setName(event.target.value)}
           placeholder="Name"
         /> */}
-        <TextField
-          required
-          className="add-movie-txt"
-          id="outlined-required"
-          label="Ratings"
-          defaultValue="Hello World"
-          value={Ratings}
-          onChange={(event) => setRatings(event.target.value)}
-          placeholder="Ratings"
-        />
-        {/* <input
+            <TextField
+              required
+              className="add-movie-txt"
+              id="outlined-required"
+              label="Ratings"
+              value={Ratings}
+              onChange={(event) => setRatings(event.target.value)}
+              placeholder="Ratings"
+            />
+            {/* <input
           value={Ratings}
           onChange={(event) => setRatings(event.target.value)}
           placeholder="Ratings"
         /> */}
-        <TextField
-          required
-          className="add-movie-txt"
-          id="outlined-required"
-          label="Image URL"
-          defaultValue="Hello World"
-          value={Image}
-          onChange={(event) => setImage(event.target.value)}
-          placeholder="Image URL"
-        />
-        {/* <input
+            <TextField
+              required
+              className="add-movie-txt"
+              id="outlined-required"
+              label="Image URL"
+              value={Image}
+              onChange={(event) => setImage(event.target.value)}
+              placeholder="Image URL"
+            />
+            {/* <input
           value={Image}
           onChange={(event) => setImage(event.target.value)}
           placeholder="Image URL"
         /> */}
 
-        <TextField
-          required
-          className="add-movie-txt"
-          id="outlined-multiline-flexible"
-          label="Summary"
-          multiline
-          maxRows={4}
-          // value={value}
-          value={Summary}
-          onChange={(event) => {
-            setSummary(event.target.value);
-            // setValue(event.target.value);
-          }}
-          placeholder="Summary"
-        />
-        {/* <input
+            <TextField
+              required
+              className="add-movie-txt"
+              id="outlined-multiline-flexible"
+              label="Summary"
+              multiline
+              maxRows={4}
+              // value={value}
+              value={Summary}
+              onChange={(event) => {
+                setSummary(event.target.value);
+                // setValue(event.target.value);
+              }}
+              placeholder="Summary"
+            />
+            {/* <input
           value={Summary}
           onChange={(event) => setSummary(event.target.value)}
           placeholder="Summary"
         /> */}
 
-        <div className="btnWrapper text-center">
-          <button
-            type="button"
-            class="btn btn-primary btnSubmit"
-            onClick={() => {
-              const newMovie = {
-                Name: Name,
-                Ratings: Ratings,
-                Image: Image,
-                Summary: Summary,
-              };
-              setMovieList([...movieList, newMovie]);
-              setAddMovieBtn(addMovieBtn === "block" ? "none" : "block");
-              setAddBtn(addBtn === "none" ? "block" : "none");
-            }}
-          >
-            Submit
-          </button>
+            <div className="btnWrapper text-center">
+              <button
+                type="button"
+                className="btn btn-primary btnSubmit"
+                onClick={() => {
+                  const newMovie = {
+                    Name: Name,
+                    Ratings: Ratings,
+                    Image: Image,
+                    Summary: Summary,
+                  };
+                  setMovieList([...movieList, newMovie]);
+                  setAddMovieBtn(addMovieBtn === "block" ? "none" : "block");
+                  setAddBtn(addBtn === "none" ? "block" : "none");
+                }}
+              >
+                Submit
+              </button>
 
-          <button
-            onClick={() => {
-              setAddMovieBtn(addMovieBtn === "block" ? "none" : "block");
-              setAddBtn(addBtn === "none" ? "block" : "none");
-            }}
-            type="button"
-            class="btn btn-primary btnCancel"
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
+              <button
+                onClick={() => {
+                  setAddMovieBtn(addMovieBtn === "block" ? "none" : "block");
+                  setAddBtn(addBtn === "none" ? "block" : "none");
+                }}
+                type="button"
+                className="btn btn-primary btnCancel"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
 
-      <section className="main-container">
-        {movieList.map((movie) => (
-          <Movie
-            Name={movie.Name}
-            Ratings={movie.Ratings}
-            Image={movie.Image}
-            Summary={movie.Summary}
-          />
-        ))}
-      </section>
+          <section className="main-container">
+            {movieList.map((movie, index) => (
+              <Movie
+                KeyID={index}
+                Name={movie.Name}
+                Ratings={movie.Ratings}
+                Image={movie.Image}
+                Summary={movie.Summary}
+              />
+            ))}
+          </section>
         </Route>
 
-        <Route path = "/">
-<h1>Welcome to Movie app</h1>
+        <Route path="/">
+          <h1>Welcome to Movie app</h1>
         </Route>
       </Switch>
+    </div>
+  );
+}
 
-      
-      
+function MovieDetils({movieList}) {
+  // console.log(movieList);
+  const { movieid } = useParams();
+  const movie = movieList[movieid];
+  const name = movie.Name;
+  const description  = movie.Summary;
+  return (
+    <div>
+      <h1>{movieid}</h1>
+      <h2>{name}</h2>
+      <h5>{description}</h5>
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -6,33 +6,39 @@ import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Counter } from "./Counter";
+import { useHistory } from "react-router-dom";
 
-export function Movie({ Name, Ratings, Image, Summary }) {
+export function Movie({ KeyID, Name, Ratings, Image, Summary }) {
   const ratingStyles = { color: Ratings < 8 ? "red" : "green" };
 
   const [visible, setVisible] = useState(false);
   const showSummaryStyle = { display: visible ? "block" : "none" };
+  // console.log(KeyID);
+  const history = useHistory();
 
   return (
-    <Card className="movie-container">
+    <Card className="movie-container" key={KeyID}>
       <CardContent>
         <img className="movie-poster" src={Image} alt=""></img>
         <Counter />
-        <hr></hr>
+        {/* <hr></hr> */}
         <section className="movie-section">
           <h3 className="movie-name">
             {Name}
             <IconButton onClick={() => setVisible(!visible)}>
               {visible ? <ExpandLessIcon /> : <ExpandMoreIcon />}
             </IconButton>
+            <IconButton
+              onClick={() => history.push("/movies/"+ KeyID)}>!</IconButton>
           </h3>
+          
           <p className="movie-rating" style={ratingStyles}>
             ⭐ {Ratings}
           </p>
         </section>
 
         <p style={showSummaryStyle} className="movie-summary">
-          <hr></hr>
+          {/* <hr></hr> */}
           {Summary}
         </p>
       </CardContent>
